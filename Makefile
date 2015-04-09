6ee569e01d269f671b701dc0cd63e11b4f342a95
Kenny Root <kenny@the-b.org>
2015-04-08 22:20:42 -0700

    Fully script the promo creation

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 GIMP=/Applications/Gimp.app/Contents/MacOS/gimp-2.8
 
+ASSETS=$(wildcard assets/*.png)
 SRCS=$(wildcard text/*.txt)
 OUTPUT=$(SRCS:text/%.txt=output/%.png)
 
-output/%.png: text/%.txt assets/connectbot-promo.xcf src/create-play-promo.scm
-	echo '(localize-subheading "$(shell cat $<)" "Essential PragmataPro" "assets/connectbot-promo.xcf" "$@") (gimp-quit 0)' | cat src/create-play-promo.scm - | $(GIMP) -n -i -b -
+output/%.png: text/%.txt src/create-play-promo.scm $(ASSETS)
+	echo '(localize-subheading "$(shell cat $<)" "Essential PragmataPro" "$@") (gimp-quit 0)' | cat src/create-play-promo.scm - | $(GIMP) -n -i -b -
 
 all: $(OUTPUT)
--- src/create-play-promo.scm
+++ src/create-play-promo.scm
@@ -1,26 +1,48 @@
-(define (localize-subheading text font input-file output-file)
-  (gimp-context-set-foreground '(255 255 255))
-  (let* ((img (car (gimp-file-load RUN-NONINTERACTIVE input-file input-file)))
-         (textlayer (car (gimp-text-fontname img -1
-                                             0 0
-                                             text
-                                             0
-                                             TRUE
-                                             51 PIXELS
-                                             font))))
-    (gimp-text-layer-set-justification textlayer TEXT-JUSTIFY-CENTER)
-    (gimp-layer-translate textlayer 406 260)
-    (gimp-text-layer-resize textlayer 556 206)
-    ; Create the drop text shadow laye)r
-    (let* ((shadowlayer (car (gimp-layer-copy textlayer TRUE))))
-      (gimp-image-add-layer img shadowlayer 3)
-      (gimp-layer-resize-to-image-size shadowlayer)
-      (gimp-layer-set-opacity shadowlayer 56)
-      (gimp-context-set-foreground '(0 0 0))
-      (gimp-image-select-item img CHANNEL-OP-ADD shadowlayer)
-      (gimp-selection-grow img 20)
-      (gimp-selection-feather img 15)
-      (gimp-bucket-fill shadowlayer FG-BUCKET-FILL NORMAL-MODE 100 0 FALSE 0 0))
-    ; Flatten the image and write it out
+(define (shadow-layer img orig-layer)
+  (let* ((shadow-layer (car (gimp-layer-copy orig-layer TRUE))))
+    (gimp-image-insert-layer img shadow-layer 0 -1)
+    (gimp-image-lower-layer img shadow-layer)
+    (gimp-layer-resize-to-image-size shadow-layer)
+    (gimp-layer-set-opacity shadow-layer 56)
+    (gimp-context-set-foreground '(0 0 0))
+    (gimp-image-select-item img CHANNEL-OP-ADD shadow-layer)
+    (gimp-selection-grow img 20)
+    (gimp-selection-feather img 15)
+    (gimp-bucket-fill shadow-layer FG-BUCKET-FILL NORMAL-MODE 100 0 FALSE 0 0)))
+  
+(define (create-text-layer img text px-size font x y width height)
+  (let* (
+    (layer (car (gimp-text-fontname img -1
+                                    0 0
+                                    text
+                                    0
+                                    TRUE
+                                    px-size PIXELS
+                                    font))))
+    (gimp-text-layer-set-justification layer TEXT-JUSTIFY-CENTER)
+    (gimp-text-layer-set-color layer '(255 255 255))
+    (gimp-layer-translate layer x y)
+    (gimp-text-layer-resize layer width height)
+    (shadow-layer img layer)
+  )
+)
+
+(define (localize-subheading subtitle font output-file)
+  (let*
+    ( 
+      (img (car (gimp-file-load RUN-NONINTERACTIVE "assets/connectbot-promo-bg.png" "assets/connectbot-promo-bg.png")))
+      (logo-layer (car (gimp-file-load-layer RUN-NONINTERACTIVE img "assets/connectbot-promo-logo.png")))
+    )
+    ; Adjust the logo
+    (gimp-image-insert-layer img logo-layer 0 -1)
+    (gimp-layer-translate logo-layer 80 (/ (- (car (gimp-drawable-height img)) (car (gimp-drawable-height logo-layer))) 2))
+    (shadow-layer img logo-layer)
+    (gimp-image-set-active-layer img logo-layer)
+
+    ; Title
+    (create-text-layer img "ConnectBot" 104 font 390 130 584 152)
+    ; Subtitle
+    (create-text-layer img subtitle 51 font 406 260 556 206)
+
     (let* ((result (car (gimp-image-flatten img))))
       (gimp-file-save RUN-NONINTERACTIVE img result output-file output-file))))
